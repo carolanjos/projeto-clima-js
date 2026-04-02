7cb2d3cc1ac898fe39cbc20911c136a05b599cbd
Carolina dos Anjos <carolinadosanjos60@gmail.com>
2026-04-02 16:25:29 -0300

    changes layout - add cards

--- app-clima/script.js
+++ app-clima/script.js
@@ -2,6 +2,7 @@ const inputCidade = document.getElementById("input-cidade");
 const btnBuscar = document.getElementById("btn-buscar");
 const resultado = document.getElementById("resultado");
 const btnLimpar = document.getElementById("btn-limpar");
+const resultadoLocalizacao = document.getElementById("resultado-localizacao");
 
 const KEY_API = "8639c5563f5c9b632e2336331b81df90";
 
@@ -19,12 +20,13 @@ async function buscarClima(cidade) {
   if (dados.cod === 200) {
     resultado.style.display = "block";
     resultado.innerHTML = `
-      <h2>${dados.name}, ${dados.sys.country}</h2>
-      <p>Temperatura: ${dados.main.temp} °C</p>
-      <p>Sensação Térmica: ${dados.main.feels_like} °C</p>
-      <p>Clima: ${dados.weather[0].description}</p>
-      <p>Umidade: ${dados.main.humidity}%</p>
-      <p>Vento: ${dados.wind.speed} m/s</p>
+      <h4>🌤️ Clima Atual</h2>
+      <h5>${dados.name}, ${dados.sys.country}</h2>
+      <p>🌡️ Temperatura: ${dados.main.temp} °C</p>
+      <p>🤔 Sensação Térmica: ${dados.main.feels_like} °C</p>
+      <p>🌤️ Clima: ${dados.weather[0].description}</p>
+      <p>💧 Umidade: ${dados.main.humidity}%</p>
+      <p>💨 Vento: ${dados.wind.speed} m/s</p>
     `;
   } else {
     resultado.style.display = "block";
@@ -61,21 +63,20 @@ async function buscarClimaPorLocalizacao(lat, lon) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY_API}&units=metric&lang=pt_br`;
 
   if (!lat || !lon) {
-    resultado.style.display = "block";
-    resultado.innerHTML = `<p>❌ Não foi possível obter sua localização. Por favor, permita o acesso à localização ou insira o nome de uma cidade.</p>`;
+    resultadoLocalizacao.style.display = "block";
+    resultadoLocalizacao.innerHTML = `<p>❌ Não foi possível obter sua localização. Por favor, permita o acesso à localização ou insira o nome de uma cidade.</p>`;
     return;
   }
 
   const response = await fetch(url);
   const position = await response.json();
 
-  console.log(position); // ← adiciona essa linha
-  console.log(position.cod); // ← e essa
-
   if (position.cod === 200) {
-    resultado.style.display = "block";
-    resultado.innerHTML = `
-      <h2>${position.name}, ${position.sys.country}</h2>
+    resultadoLocalizacao.style.display = "block";
+    resultadoLocalizacao.innerHTML = `
+      <h3>🌍 Clima Atual da Sua Localização: </h3>
+      <br>
+      <h3>${position.name}, ${position.sys.country}</h3>
       <p>🌡️ Temperatura: ${position.main.temp}°C</p>
       <p>🤔 Sensação térmica: ${position.main.feels_like}°C</p>
       <p>💧 Umidade: ${position.main.humidity}%</p>
